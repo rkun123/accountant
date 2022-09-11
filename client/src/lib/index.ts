1dbf162d9ec0b5637b5dc38c491c210b3ddf2647
rkun123 <rkun123@hotmail.com>
2022-09-11 06:05:44 +0000

    update: read env

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createSignal } from "react-signal";
 
+const url: string = import.meta.env.VITE_API_URL;
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/query",
   cache: new InMemoryCache(),
