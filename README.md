1a0acc0db4e87d9da657d577db99bd995902071d
rkun123 <tren111213@gmail.com>
2022-11-02 21:45:44 +0900

    update: vite.config.ts and README.md

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ docker compose exec sql-migrate up
 ```bash
 docker compose exec db mysql -h db -u accountant -p accountant
 ```
+
+# 機能追加
+## 1. `graph/schema.resolvers.go` に定義を追加
+`type` / `input` / `query` / `Mutation` に定義を追加していく
+
+## 2. `graph/operations.graphql` に定義を追加
+`query`で定義を追加
+
+## 3. Go側の型とresolver定義を生成
+1. `go run github.com/99designs/gqlgen generate`
+2. `cd client && yarn generate`
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { env } from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/query": "https://mio.chinchinmogamo.ga",
+      "/query": env["ACCOUNTANT_ENV"] === 'production' ? "https://mio.chinchinmogamo.ga" : "http://localhost:8080",
     },
   },
 });
